355fddd9ec70f03f9f02c9f901d9e4324a391f0a
lhitchon <larryhitchon@gmail.com>
2018-03-18 23:33:34 -0700

    clean up security group example rules, add ipv6

--- example-files/config/security_group.tf
+++ example-files/config/security_group.tf
@@ -19,6 +19,27 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
+resource "aws_security_group" "allow_all_v6" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
 resource "aws_security_group" "allow_all_for_ssh" {
   name        = "allow_all"
   description = "Allow all inbound traffic"
