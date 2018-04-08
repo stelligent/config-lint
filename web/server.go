1119c2558a234a5d328929d13833e839c90437f3
lhitchon <larry.hitchon@stelligent.com>
2018-04-07 17:48:15 -0700

    add Dockerfile-web

--- web/server.go
+++ web/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stelligent/config-lint/assertion"
 	"github.com/stelligent/config-lint/linter"
@@ -44,7 +43,6 @@ func lintResults(c *gin.Context) {
 		return
 	}
 	var config = c.PostForm("config")
-	fmt.Println("config:", config)
 	f, err := ioutil.TempFile("/tmp", "lint")
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"Message": err.Error()})
