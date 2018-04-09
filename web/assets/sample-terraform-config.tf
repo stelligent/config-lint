4d5a064d45a9a9f0ee2b2cd5e2ad62a04000a27f
lhitchon <larry.hitchon@stelligent.com>
2018-04-09 13:35:54 -0700

    fix Terraform rules, example config for web

--- web/assets/sample-terraform-config.tf
+++ web/assets/sample-terraform-config.tf
@@ -1,10 +1,19 @@
+resource "aws_kms_key" "key_for_s3_encryption" {
+  description = "key for S3 bucket encryption"
+}
+
 resource "aws_s3_bucket" "bucket_example_1" {
-  bucket = "my-bucket-1"
   acl = "public-read"
 }
 
 resource "aws_s3_bucket" "bucket_example_2" {
-  bucket = "my-bucket-2"
   acl = "public-read-write"
-  encrypted = false
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.key_for_s3_encryption.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 }
