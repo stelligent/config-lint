f321d87463420f51721d7bebd547d501fbf24e5f
Larry Hitchon <larry.hitchon@stelligent.com>
2018-07-08 13:08:17 -0700

    add test for value_from HTTP endpoint

--- assertion/value_test.go
+++ assertion/value_test.go
@@ -1,6 +1,10 @@
 package assertion
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -18,4 +22,19 @@ func TestCommandLineVariable(t *testing.T) {
 	if v != "bar" {
 		t.Errorf("Expected GetValue to find variable 'foo' with value 'bar', not '%s'\n", v)
 	}
+}
+
+func TestValueFromHttp(t *testing.T) {
+	cidrBlock := "0.0.0.0/0"
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, cidrBlock)
+	}))
+	defer ts.Close()
+	s := StandardValueSource{}
+	e := Expression{
+		ValueFrom: ValueFrom{URL: ts.URL},
+	}
+	v, err := s.GetValue(e)
+	assert.Nil(t, err, "Expecting GetValue to not return an error")
+	assert.Equal(t, cidrBlock, v, "Expecting CIDR value to be returned")
 }
