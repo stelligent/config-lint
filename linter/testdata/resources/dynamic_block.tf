6bb837e3c702bec2db78f527620a21f6b0a0aaf4
Jim Rohrer <jrohrer1@gmail.com>
2020-01-07 09:20:15 -0600

    Added test for TF 12 Dynamic Blocks and an example to the terraform.md readme

---
+++ linter/testdata/resources/dynamic_block.tf
@@ -0,0 +1,18 @@
+variable "service_ports" {
+  default = [22, 80, 1433, 6379]
+}
+
+resource "aws_security_group" "example" {
+  name = "example"
+
+  dynamic "ingress" {
+    for_each = var.service_ports
+    content {
+      from_port = ingress.value
+      to_port   = ingress.value
+      protocol  = "tcp"
+    }
+  }
+
+  egress = "-1"
+}
