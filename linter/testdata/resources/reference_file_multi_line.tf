647f7baa7d0f303baca765601cd733c6a70a722c
Trey McElhattan <trey.mcelhattan@stelligent.com>
2020-01-08 09:06:59 -0500

    cleaning up file

--- linter/testdata/resources/reference_file_multi_line.tf
+++ linter/testdata/resources/reference_file_multi_line.tf
@@ -1,14 +1,6 @@
-//resource "aws_s3_bucket" "a_bucket" {
-//  bucket = "${file("./testdata/data/multi_line_content")}"
-//}
-
 terraform {
   required_version = ">= 0.12.0"
 }
-
-//data "template_file" "multi_line" {
-//  template = file("./testdata/data/multi_line_content")
-//}
 
 resource "test_resource" "test" {
   test_value = file("./testdata/data/multi_line_content")
