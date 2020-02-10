491dd7de8a3f3d88dfc79f3eacb501448e535305
Peter Helewski <peter.helewski@stelligent.com>
2020-02-10 18:50:59 +0000

    adding docker hub login for vscode-remote push

--- .devcontainer/build/dockerhub.sh
+++ .devcontainer/build/dockerhub.sh
@@ -1,16 +1,31 @@
 #!/bin/bash -ex
 
 set +x
+if [[ -z ${DOCKER_USER} ]];
+then
+  echo DOCKER_USER must be set in the environment
+  exit 1
+fi
+if [[ -z ${DOCKER_PASSWORD} ]];
+then
+  echo DOCKER_PASSWORD must be set in the environment
+  exit 1
+fi
 if [[ -z ${DOCKER_ORG} ]];
 then
-  echo $DOCKER_ORG must be set in the environment
+  echo DOCKER_ORG must be set in the environment
   exit 1
 fi
 if [[ -z ${NEW_VERSION} ]];
 then
-  echo $NEW_VERSION must be set in the environment
+  echo NEW_VERSION must be set in the environment
   exit 1
 fi
+set -x
+
+# Docker Login
+set +x
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
 set -x
 
 # publish vscode-remote docker image to DockerHub, https://hub.docker.com/r/stelligent/vscode-remote-config-lint
