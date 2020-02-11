cf805e561d45799749efd1e4c33ab016f88e0ff6
Peter Helewski <peter.helewski@stelligent.com>
2020-02-11 21:44:31 +0000

    adding separate workflow to build and push vscode remote dev image to dockerhub

--- .devcontainer/build/dockerhub.sh
+++ .devcontainer/build/dockerhub.sh
@@ -6,15 +6,17 @@ then
   echo DOCKER_ORG must be set in the environment
   exit 1
 fi
-if [[ -z ${NEW_VERSION} ]];
+if [[ -z ${GITHUB_SHA} ]];
 then
-  echo NEW_VERSION must be set in the environment
+  echo GITHUB_SHA must be set in the environment
   exit 1
 fi
 set -x
 
+COMMIT_HASH=${GITHUB_SHA:0:8}
+
 # publish vscode-remote docker image to DockerHub, https://hub.docker.com/r/stelligent/vscode-remote-config-lint
-docker build -t $DOCKER_ORG/vscode-remote-config-lint:${NEW_VERSION} --file .devcontainer/build/Dockerfile .
-docker tag $DOCKER_ORG/vscode-remote-config-lint:${NEW_VERSION} $DOCKER_ORG/vscode-remote-config-lint:latest
-docker push $DOCKER_ORG/vscode-remote-config-lint:${NEW_VERSION}
+docker build -t $DOCKER_ORG/vscode-remote-config-lint:${COMMIT_HASH} --file .devcontainer/build/Dockerfile .
+docker tag $DOCKER_ORG/vscode-remote-config-lint:${COMMIT_HASH} $DOCKER_ORG/vscode-remote-config-lint:latest
+docker push $DOCKER_ORG/vscode-remote-config-lint:${COMMIT_HASH}
 docker push $DOCKER_ORG/vscode-remote-config-lint:latest
