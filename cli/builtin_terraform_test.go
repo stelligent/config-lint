2e050e25f93cfa5887e885929a18a516e3d8b5bd
Peter Helewski <peter.helewski@stelligent.com>
2020-02-14 21:05:18 +0000

    adding rule and test cases for aws_redshift_parameter_group require_ssl parameter

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -106,6 +106,7 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"kinesis.tf", "KINESIS_FIREHOSE_DELIVERY_STREAM_ENCRYPTION", 0, 1},
 		{"redshift_encryption.tf", "REDSHIFT_CLUSTER_ENCRYPTION", 0, 2},
 		{"redshift_kms.tf", "REDSHIFT_CLUSTER_KMS_KEY_ID", 1, 0},
+		{"redshift_parameter_group_require_ssl.tf", "REDSHIFT_CLUSTER_PARAMETER_GROUP_REQUIRE_SSL", 2, 0},
 		{"ecs.tf", "ECS_ENVIRONMENT_SECRETS", 0, 1},
 	}
 	for _, tc := range testCases {
