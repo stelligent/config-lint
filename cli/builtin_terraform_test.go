cf8f482982b5067e03adcf4d613f2d049f5e4462
Peter Helewski <peter.helewski@stelligent.com>
2020-02-17 14:57:17 +0000

    restructuring redshift_cluster test items

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -104,9 +104,9 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"db.tf", "RDS_CLUSTER_ENCYPTION", 0, 2},
 		{"efs.tf", "EFS_ENCRYPTED", 0, 1},
 		{"kinesis.tf", "KINESIS_FIREHOSE_DELIVERY_STREAM_ENCRYPTION", 0, 1},
-		{"redshift_encryption.tf", "REDSHIFT_CLUSTER_ENCRYPTION", 0, 2},
-		{"redshift_kms.tf", "REDSHIFT_CLUSTER_KMS_KEY_ID", 1, 0},
-		{"redshift_public_access.tf", "REDSHIFT_CLUSTER_PUBLICLY_ACCESSIBLE", 0, 2},
+		{"redshift_cluster/encrypted.tf", "REDSHIFT_CLUSTER_ENCRYPTION", 0, 2},
+		{"redshift_cluster/kms_key_id.tf", "REDSHIFT_CLUSTER_KMS_KEY_ID", 1, 0},
+		{"redshift_cluster/publicly_accessible.tf", "REDSHIFT_CLUSTER_PUBLICLY_ACCESSIBLE", 0, 2},
 		{"ecs.tf", "ECS_ENVIRONMENT_SECRETS", 0, 1},
 	}
 	for _, tc := range testCases {
