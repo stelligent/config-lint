c2ae64373092475ec6b77d046915d1449b372063
Peter Helewski <peter.helewski@stelligent.com>
2020-02-18 19:17:48 +0000

    adding tests for IAM_ROLE_POLICY_WILDCARD_ACTION rule

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -63,7 +63,7 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"aws/iam_role/assume_role_policy_action_wildcard.tf", "IAM_ROLE_WILDCARD_ACTION", 0, 1},
 		{"aws/iam_role_policy/notaction.tf", "IAM_ROLE_POLICY_NOT_ACTION", 1, 0},
 		{"aws/iam_role_policy/notresource.tf", "IAM_ROLE_POLICY_NOT_RESOURCE", 1, 0},
-		{"aws/iam.tf", "IAM_ROLE_POLICY_WILDCARD_ACTION", 0, 0},
+		{"aws/iam_role_policy/action_wildcard.tf", "IAM_ROLE_POLICY_WILDCARD_ACTION", 0, 1}, // TODO: fix file naming to include policy_
 		{"aws/iam.tf", "IAM_ROLE_POLICY_WILDCARD_RESOURCE", 0, 0},
 		{"aws/iam.tf", "IAM_POLICY_NOT_ACTION", 0, 0},
 		{"aws/iam.tf", "IAM_POLICY_NOT_RESOURCE", 0, 0},
