2957d42c5de8f9a6e2ac243a444b738537d7ae99
Peter Helewski <peter.helewski@stelligent.com>
2020-02-18 12:36:38 +0000

    creating tests for egress port range matching rule

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -51,7 +51,7 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"aws/security_group/rdp_world_ingress.tf", "SG_RDP_WORLD_INGRESS", 0, 2},
 		{"aws/security_group/non_32_ingress.tf", "SG_NON_32_INGRESS", 2, 0},
 		{"aws/security_group/ingress_port_range.tf", "SG_INGRESS_PORT_RANGE", 1, 0},
-		{"aws/security-groups.tf", "SG_EGRESS_PORT_RANGE", 0, 0},
+		{"aws/security_group/egress_port_range.tf", "SG_EGRESS_PORT_RANGE", 1, 0},
 		{"aws/security-groups.tf", "SG_MISSING_EGRESS", 0, 0},
 		{"aws/security-groups.tf", "SG_INGRESS_ALL_PROTOCOLS", 1, 0},
 		{"aws/security-groups.tf", "SG_EGRESS_ALL_PROTOCOLS", 3, 0},
