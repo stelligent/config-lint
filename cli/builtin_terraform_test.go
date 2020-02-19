8ba5aa4e59532c64a61818fafa9e901342c01787
Peter Helewski <peter.helewski@stelligent.com>
2020-02-19 20:16:50 +0000

    adding tests for S3_BUCKET_ACL rule

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -76,7 +76,7 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"aws/iam_role_policy/policy_version.tf", "POLICY_VERSION", 0, 1},
 		{"aws/iam_role/assume_role_policy_version.tf", "ASSUME_ROLEPOLICY_VERSION", 0, 1},
 		{"aws/elb/access_logs_enabled.tf", "ELB_ACCESS_LOGGING", 2, 0},
-		{"aws/s3.tf", "S3_BUCKET_ACL", 0, 0},
+		{"aws/s3_bucket/acl_not_public.tf", "S3_BUCKET_ACL", 0, 2},
 		{"aws/s3.tf", "S3_NOT_ACTION", 0, 0},
 		{"aws/s3.tf", "S3_NOT_PRINCIPAL", 0, 0},
 		{"aws/s3_bucket_policy/policy_version.tf", "POLICY_VERSION", 0, 1},
