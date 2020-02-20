b8233d0a3adb44bb009d467ae7a269f491ac4d54
Peter Helewski <peter.helewski@stelligent.com>
2020-02-20 13:01:35 +0000

    fixing assertion and adding tests for SNS_TOPIC_POLICY_NOT_PRINCIPAL

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -88,7 +88,7 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"aws/sns_topic_policy/policy_version.tf", "POLICY_VERSION", 0, 1},
 		{"aws/sns_topic_policy/policy_statement_principal_wildcard-copy.tf", "SNS_TOPIC_POLICY_WILDCARD_PRINCIPAL", 0, 1},
 		{"aws/sns_topic_policy/policy_statement_notaction.tf", "SNS_TOPIC_POLICY_NOT_ACTION", 1, 0},
-		{"aws/sns.tf", "SNS_TOPIC_POLICY_NOT_PRINCIPAL", 0, 0},
+		{"aws/sns_topic_policy/policy_statement_notprincipal.tf", "SNS_TOPIC_POLICY_NOT_PRINCIPAL", 1, 0},
 		{"aws/sqs_queue_policy/policy_version.tf", "POLICY_VERSION", 0, 1},
 		{"aws/sqs.tf", "SQS_QUEUE_POLICY_WILDCARD_PRINCIPAL", 1, 0},
 		{"aws/sqs.tf", "SQS_QUEUE_POLICY_WILDCARD_ACTION", 0, 0},
