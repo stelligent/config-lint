ef7e9b6c72b71208154ab038c8cbff1596f55739
Peter Helewski <peter.helewski@stelligent.com>
2020-02-21 17:58:13 +0000

    changing severity level and creating tests for CLOUDTRAIL_ENCRYPTION rule

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -110,7 +110,7 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"aws/ami_copy/encrypted.tf", "AMI_COPY_SNAPSHOTS_ENCRYPTED", 0, 2},
 		{"aws/ec2.tf", "EBS_BLOCK_DEVICE_ENCRYPTED", 0, 0},
 		{"aws/ec2.tf", "EBS_VOLUME_ENCRYPTION", 0, 2},
-		{"aws/cloudtrail.tf", "CLOUDTRAIL_ENCRYPTION", 0, 1},
+		{"aws/cloudtrail/kms_key_id.tf", "CLOUDTRAIL_ENCRYPTION", 1, 0},
 		{"aws/codebuild_project/project_encryption.tf", "CODEBUILD_PROJECT_ENCRYPTION", 0, 1},
 		{"aws/codebuild_project/artifact_encryption.tf", "CODEBUILD_PROJECT_ARTIFACT_ENCRYPTION", 0, 3},
 		{"aws/codepipeline.tf", "CODEPIPELINE_ENCRYPTION", 0, 0},
