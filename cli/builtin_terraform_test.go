26654fccd06b2f869e6a122451968f21eeb291d2
Peter Helewski <peter.helewski@stelligent.com>
2020-02-21 14:45:33 +0000

    creating alb tests for ALB_LISTENER_HTTPS rule. commenting out ssl_policy values for now.

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -100,7 +100,7 @@ func TestTerraformBuiltInRules(t *testing.T) {
 		{"aws/lambda.tf", "LAMBDA_FUNCTION_ENCRYPTION", 1, 0},
 		{"aws/lambda.tf", "LAMBDA_ENVIRONMENT_SECRETS", 0, 1},
 		{"aws/waf_web_acl/default_action_type.tf", "WAF_WEB_ACL", 0, 1},
-		{"aws/alb.tf", "ALB_LISTENER_HTTPS", 0, 3},
+		{"aws/alb_listener/https.tf", "ALB_LISTENER_HTTPS", 0, 4},
 		{"aws/alb.tf", "ALB_ACCESS_LOGS", 0, 0},
 		{"aws/ami.tf", "AMI_VOLUMES_ENCRYPTED", 0, 1},
 		{"aws/ami.tf", "AMI_COPY_SNAPSHOTS_ENCRYPTED", 0, 1},
