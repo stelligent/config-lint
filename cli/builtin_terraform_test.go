57da347d6620fe41aaed391df5b4c744f61b5538
Peter Helewski <peter.helewski@stelligent.com>
2020-02-25 15:32:38 +0000

    added AWS_DMS_ENDPOINT_ENCRYPTION to test suite and created tests to validate rule

--- cli/builtin_terraform_test.go
+++ cli/builtin_terraform_test.go
@@ -177,6 +177,7 @@ func RunTestTerraformBuiltInRules(t *testing.T, terraformVersion string) {
 		{"tf", "aws/elasticache_replication_group/encryption_in_transit.tf", "ELASTICACHE_ENCRYPTION_TRANSIT", 0, 2},
 		{"tf", "aws/batch_job_definition/container_properties_privileged.tf", "BATCH_DEFINITION_PRIVILEGED", 1, 0},
 		{"tf", "aws/neptune_cluster/encryption.tf", "NEPTUNE_DB_ENCRYPTION", 0, 2},
+		{"tf", "aws/dms_endpoint/kms_key.tf", "AWS_DMS_ENDPOINT_ENCRYPTION", 1, 0},
 	}
 
 	// Run test cases
