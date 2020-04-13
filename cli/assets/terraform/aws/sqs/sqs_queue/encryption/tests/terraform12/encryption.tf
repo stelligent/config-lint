33dd4a7e58045405f960c88035d665e8f6c59e96
Keith Monihen <kmonihen@hosting.com>
2020-04-13 10:54:48 -0400

    added better comments for sqs queue rule

--- cli/assets/terraform/aws/sqs/sqs_queue/encryption/tests/terraform12/encryption.tf
+++ cli/assets/terraform/aws/sqs/sqs_queue/encryption/tests/terraform12/encryption.tf
@@ -1,5 +1,10 @@
 # Test that sqs queue has kms_master_key_id and kms_data_key_reuse_period_seconds defined
-#
+# https://www.terraform.io/docs/providers/aws/r/sqs_queue.html#kms_master_key_id
+# https://www.terraform.io/docs/providers/aws/r/sqs_queue.html#kms_data_key_reuse_period_seconds
+
+provider "aws" {
+  region = "us-east-1"
+}
 
 # Pass: both kms_master_key_id and kms_data_key_reuse_period_seconds are used
 resource "aws_sqs_queue" "encryption_enabled_with_reuse_set" {
@@ -13,7 +18,7 @@ resource "aws_sqs_queue" "encryption_disabled" {
   name = "queue-without-encryption"
 }
 
-# Fail: 
+# Fail: kms key defined without kms_data_key_reuse_period_seconds
 resource "aws_sqs_queue" "encryption_without_reuse_period" {
   name              = "queue-without-encryption"
   kms_master_key_id = "alias/foo/bar"
