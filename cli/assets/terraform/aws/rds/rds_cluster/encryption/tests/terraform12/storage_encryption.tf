6f7993608879fff340d834c5c4cb4bc0931680c2
Keith Monihen <kmonihen@hosting.com>
2020-04-13 19:58:34 -0400

    Warn when rds cluster is not using encryption

--- cli/assets/terraform/aws/rds/rds_cluster/encryption/tests/terraform12/storage_encryption.tf
+++ cli/assets/terraform/aws/rds/rds_cluster/encryption/tests/terraform12/storage_encryption.tf
@@ -1,12 +1,21 @@
-# Pass
+# Test that an RDS cluster has encryption endabled and a KMS key specified
+# https://www.terraform.io/docs/providers/aws/r/rds_cluster.html#storage_encrypted
+# https://www.terraform.io/docs/providers/aws/r/rds_cluster.html#kms_key_id
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# PASS: storage_encrypted enabled and kms_key_id specified
 resource "aws_rds_cluster" "storage_encrypted_set_to_true" {
   engine            = "aurora-mysql"
   master_username   = "foo"
   master_password   = "bar"
   storage_encrypted = true
+  kms_key_id        = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
 }
 
-# Fail
+# FAIL: storage_encrypted disabled
 resource "aws_rds_cluster" "storage_encrypted_set_to_false" {
   engine            = "aurora-mysql"
   master_username   = "foo"
@@ -14,14 +23,14 @@ resource "aws_rds_cluster" "storage_encrypted_set_to_false" {
   storage_encrypted = false
 }
 
-# Fail
+# FAIL: storage_encrypted not defined
 resource "aws_rds_cluster" "storage_encrypted_not_set" {
   engine          = "aurora-mysql"
   master_username = "foo"
   master_password = "bar"
 }
 
-# Pass
+# PASS: serverless mode uses default encryption
 resource "aws_rds_cluster" "serverless_engine_mode_encrypted_by_default" {
   engine          = "aurora-mysql"
   engine_mode     = "serverless"
@@ -29,8 +38,18 @@ resource "aws_rds_cluster" "serverless_engine_mode_encrypted_by_default" {
   master_password = "bar"
 }
 
-# Pass
+# PASS: serverless mode with encryption enabled and a kms key
 resource "aws_rds_cluster" "serverless_engine_mode_with_storage_encrypted_set_to_true" {
+  engine            = "aurora-mysql"
+  engine_mode       = "serverless"
+  master_username   = "foo"
+  master_password   = "bar"
+  storage_encrypted = true
+  kms_key_id        = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
+}
+
+# WARN: serverless mode with encryption enabled but no kms key
+resource "aws_rds_cluster" "serverless_engine_mode_with_storage_encrypted_set_to_true_no_kms" {
   engine            = "aurora-mysql"
   engine_mode       = "serverless"
   master_username   = "foo"
@@ -38,7 +57,7 @@ resource "aws_rds_cluster" "serverless_engine_mode_with_storage_encrypted_set_to
   storage_encrypted = true
 }
 
-# Fail
+# FAIL: encryption is disabled
 resource "aws_rds_cluster" "serverless_engine_mode_with_storage_encrypted_set_to_false" {
   engine            = "aurora-mysql"
   engine_mode       = "serverless"
@@ -47,7 +66,7 @@ resource "aws_rds_cluster" "serverless_engine_mode_with_storage_encrypted_set_to
   storage_encrypted = false
 }
 
-# Fail
+# FAIL: Encryption not enabled
 resource "aws_rds_cluster" "provisioned_engine_mode_unencrypted_by_default" {
   engine          = "aurora-mysql"
   engine_mode     = "provisioned"
@@ -55,8 +74,18 @@ resource "aws_rds_cluster" "provisioned_engine_mode_unencrypted_by_default" {
   master_password = "bar"
 }
 
-# Pass
+# PASS: Encryption enabled with kms key
 resource "aws_rds_cluster" "provisioned_engine_mode_with_storage_encrypted_set_to_true" {
+  engine            = "aurora-mysql"
+  engine_mode       = "provisioned"
+  master_username   = "foo"
+  master_password   = "bar"
+  storage_encrypted = true
+  kms_key_id        = "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab"
+}
+
+# WARN: Encryption enabled without kms key
+resource "aws_rds_cluster" "provisioned_engine_mode_with_storage_encrypted_set_to_true_no_kms" {
   engine            = "aurora-mysql"
   engine_mode       = "provisioned"
   master_username   = "foo"
@@ -64,7 +93,7 @@ resource "aws_rds_cluster" "provisioned_engine_mode_with_storage_encrypted_set_t
   storage_encrypted = true
 }
 
-# Fail
+# FAIL: Encrypytion disabled
 resource "aws_rds_cluster" "provisioned_engine_mode_with_storage_encrypted_set_to_false" {
   engine            = "aurora-mysql"
   engine_mode       = "provisioned"
