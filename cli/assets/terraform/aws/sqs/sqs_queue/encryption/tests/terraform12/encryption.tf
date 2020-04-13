77d759121862fd4af3ed138b0a4aa28a2f4748d9
Keith Monihen <kmonihen@hosting.com>
2020-04-13 10:44:14 -0400

    updated sqs encryption rule to check for reuse period

--- cli/assets/terraform/aws/sqs/sqs_queue/encryption/tests/terraform12/encryption.tf
+++ cli/assets/terraform/aws/sqs/sqs_queue/encryption/tests/terraform12/encryption.tf
@@ -1,17 +1,20 @@
-# Pass
-resource "aws_sqs_queue" "encryption_enabled" {
-  name              = "queue-with-encryption"
-  kms_master_key_id = "alias/foo/bar"
-}
+# Test that sqs queue has kms_master_key_id and kms_data_key_reuse_period_seconds defined
+#
 
-# Pass
+# Pass: both kms_master_key_id and kms_data_key_reuse_period_seconds are used
 resource "aws_sqs_queue" "encryption_enabled_with_reuse_set" {
   name                              = "queue-with-encryption"
   kms_master_key_id                 = "alias/foo/bar"
   kms_data_key_reuse_period_seconds = 60
 }
 
-# Fail
+# Fail: neither kms_master_key_id or kms_data_key_reuse_period_seconds are used
 resource "aws_sqs_queue" "encryption_disabled" {
   name = "queue-without-encryption"
+}
+
+# Fail: 
+resource "aws_sqs_queue" "encryption_without_reuse_period" {
+  name              = "queue-without-encryption"
+  kms_master_key_id = "alias/foo/bar"
 }
