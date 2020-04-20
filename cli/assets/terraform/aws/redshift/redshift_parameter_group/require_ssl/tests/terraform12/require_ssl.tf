b255773e03e37842b39b630dc597e3d0e271b629
Keith Monihen <kmonihen@hosting.com>
2020-04-20 16:28:51 -0400

    Updating rules for redshift to handle lists of parameters

--- cli/assets/terraform/aws/redshift/redshift_parameter_group/require_ssl/tests/terraform12/require_ssl.tf
+++ cli/assets/terraform/aws/redshift/redshift_parameter_group/require_ssl/tests/terraform12/require_ssl.tf
@@ -1,27 +1,50 @@
-# Warn
+# Test that require_ssl parameter is present and set to true
+# https://www.terraform.io/docs/providers/aws/r/redshift_parameter_group.html
+
+# WARN require_ssl is not set
 resource "aws_redshift_parameter_group" "parameter_and_require_ssl_not_set" {
   name   = "foobar"
   family = "redshift-1.0"
 }
 
-# Warn
+# WARN: require_ssl is false
 resource "aws_redshift_parameter_group" "require_ssl_set_to_false" {
   name   = "foobar"
   family = "redshift-1.0"
 
   parameter {
+    name  = "enable_user_activity_logging"
+    value = "true"
+  }
+
+  parameter {
     name  = "require_ssl"
     value = "false"
   }
+
+  parameter {
+    name  = "query_group"
+    value = "example"
+  }
 }
 
-# Pass
+# PASS: require_ssl is set to true
 resource "aws_redshift_parameter_group" "require_ssl_set_to_true" {
   name   = "foobar"
   family = "redshift-1.0"
 
   parameter {
+    name  = "enable_user_activity_logging"
+    value = "true"
+  }
+
+  parameter {
     name  = "require_ssl"
     value = "true"
+  }
+
+  parameter {
+    name  = "query_group"
+    value = "example"
   }
 }
